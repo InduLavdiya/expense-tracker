bd064420b32f96cd10c34ebf109d7058e83f76bd
indu <lavdiyaindu@gmail.com>
2026-07-29 19:45:02 +0530

    Remove duplicate frontend folder

--- frontend - Copy/js/api.js
+++
@@ -1,8 +0,0 @@
-// =====================================
-// API Configuration
-// =====================================
-
-const BASE_URL = "https://expense-tracker-sn96.onrender.com/api";
-
-// Token
-const token = localStorage.getItem("token");
